be39f460a3d626b44d52d22cc82f2cb6dba8e2f6
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-02-24 09:14:29 -0700

    Add tests and code for iteration 1

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,12 @@
 class Book
-  
+  attr_reader :title, :author, :publication_year
+
+  def initialize(book_details)
+    @author_first_name = book_details[:author_first_name]
+    @author_last_name = book_details[:author_last_name]
+    @title = book_details[:title]
+    @publication_date = book_details[:publication_date]
+    @author = @author_first_name + ' ' + @author_last_name
+    @publication_year = @publication_date.split.last
+  end
 end
