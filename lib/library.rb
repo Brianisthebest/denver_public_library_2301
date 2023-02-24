af9eceb16347d51f6daff581fcb8ea0e4233103d
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-02-24 10:32:14 -0700

    Add passing code for iteration 3

--- lib/library.rb
+++ lib/library.rb
@@ -14,4 +14,17 @@ class Library
   end
   @authors
  end
+
+ def publication_time_frame_for(author)
+  @time_frame_hash = Hash.new
+  start_year = author.books.min_by do |book|
+    book.publication_year
+  end
+  end_year = author.books.max_by do |book|
+    book.publication_year
+  end
+    @time_frame_hash[:start] = start_year.publication_year
+    @time_frame_hash[:end] = end_year.publication_year
+    @time_frame_hash
+  end
 end
